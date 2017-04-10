a815b2311a285fddb16e995442b4f3869f61f169
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-10 15:53:57 +0200

    Exercise Box: creates constructor of the cuboid

---
+++ box/src/Cuboid.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Adam on 2017. 04. 10..
+ */
+public class Cuboid {
+  // Create a class that represents a cuboid:
+  // It should take its three dimensions as constructor parameters (numbers)
+  // It should have a method called `getSurface` that returns the cuboid's surface
+  // It should have a method called `getVolume` that returns the cuboid's volume
+
+  private int a;
+  private int b;
+  private int c;
+
+  public Cuboid (int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  
+}
